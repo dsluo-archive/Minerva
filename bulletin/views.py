de72a03927bda57009af9cddafc6434dfbc5e8a8
Robert Stone <rstone13@uga.edu>
2018-12-09 14:37:46 -0500

    The index view for the bulletin app now displays a list of subjects from the database.

--- bulletin/views.py
+++ bulletin/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.template import loader
+
+from base.models import SubjectArea
 
 # Create your views here.
+
+# Displays the index of the bulletin view. Includes a list of subject areas.
 def index(request):
-    return render(request, 'bulletin/index.html')
+    # Obtain a list of subject areas from the database.
+    list_of_subjects = SubjectArea.objects.order_by('long')
+
+    # Create a dictionary containing the list.
+    context = {
+        'list_of_subjects': list_of_subjects,
+    }
+
+    # Pass the dictionary into the template for the index page.
+    return render(request, 'bulletin/index.html', context)
 
 def subject(request, subject):
     return render(request, 'bulletin/subject.html')
