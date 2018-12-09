8526dfb84ae33ef0a18ce9014cb25080e6c49550
Robert Stone <rstone13@uga.edu>
2018-12-09 15:57:59 -0500

    The subject page now displays the short and long name for the subject. Gives error if subject doesn't exist.

--- bulletin/views.py
+++ bulletin/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.template import loader
+
+from django.db.models.functions import Lower
 
 from base.models import SubjectArea
 
 # Create your views here.
+
 
 # Displays the index of the bulletin view. Includes a list of subject areas.
 def index(request):
@@ -18,8 +20,33 @@ def index(request):
     # Pass the dictionary into the template for the index page.
     return render(request, 'bulletin/index.html', context)
 
-def subject(request, subject):
-    return render(request, 'bulletin/subject.html')
+
+# Displays the details page for a subject.
+# This view includes a description of the subject and list of classes belonging
+# to the subject.
+def subject(request, subject_short):
+
+    # Attempt to retrieve the subject object corresponding to the value passed
+    # into the function by the URL.
+    subject_object_list = SubjectArea.objects.filter(short__iexact=subject_short)
+
+    if subject_object_list.count() == 0:  # then the subject doesn't exist.
+
+        # Renders the template without a subject object. Template will display
+        # an error message to the user.
+        return render(request, 'bulletin/subject.html')
+
+    else:  # the subject does exist.
+
+        # Create a dictionary containing the subject object.
+        context = {
+            'subject': subject_object_list[0]
+        }
+
+        # Pass the object into the template for the subject view page.
+        return render(request, 'bulletin/subject.html', context)
+
 
 def course(request, subject, course):
     return render(request, 'bulletin/course.html')
+
