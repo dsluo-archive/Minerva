ca3eb9b96d60daa8e6ca328c2b9fc13e773a512a
David Luo <mail@dsluo.me>
2018-12-09 04:21:24 -0500

    login/logout in navbar

--- base/context_processors.py
+++ base/context_processors.py
@@ -1,10 +1,18 @@
+from django.http import HttpRequest
 from django.urls import reverse
 
 
-def navbar_context(_):
-    return {
+def navbar_context(request: HttpRequest):
+    context = {
         'pages': {
             'Home':     reverse('main:index'),
-            'Schedule': 'TODO'
+            'Schedule': 'TODO',
         }
     }
+
+    if request.user.is_authenticated:
+        context['pages']['Login'] = reverse('main:users:login')
+    else:
+        context['pages']['Logout'] = reverse('main:users:logout')
+
+    return context
