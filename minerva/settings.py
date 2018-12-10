2922255757562e72c341a75457607e2d6951ad95
David Luo <mail@dsluo.me>
2018-12-10 05:44:26 -0500

    the rest of the password reset flow

--- minerva/settings.py
+++ minerva/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'main:index'
 # LOGOUT_REDIRECT_URL = 'main:index'
+
+from .secrets import EMAIL_BACKEND, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS
