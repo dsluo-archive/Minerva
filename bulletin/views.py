a4f16efdf9c2f1fa9fbc9409e78c2cf45e6ed6bf
David Luo <mail@dsluo.me>
2018-12-11 01:47:59 -0500

    prettier index and subject view. subject view now ordered by number.

--- bulletin/views.py
+++ bulletin/views.py
@@ -19,15 +19,22 @@ def subject(request, subject_short):
     """
     try:
         subject = SubjectArea.objects.get(short__iexact=subject_short)
-        courses = Course.objects.filter(subject_area=subject)
+        courses = Course.objects.filter(subject_area=subject).order_by('course_number')
         return render(request, 'bulletin/subject.html', {'subject': subject, 'courses': courses})
     except SubjectArea.DoesNotExist:
         return render(request, 'bulletin/subject.html')
 
 
+def first_match(function, iterable):
+    try:
+        return next(filter(function, iterable))
+    except StopIteration:
+        return None
+
 def course(request, subject_short, course_number):
     try:
         courses = Course.objects.filter(subject_area__short__iexact=subject_short, course_number=course_number)
+        suffix = first_match(lambda x: x, )
         return render(request, 'bulletin/course.html', {'courses': courses})
     except Course.DoesNotExist:
         return render(request, 'bulletin/course.html', {'error': "The requested course doesn't exist."})
