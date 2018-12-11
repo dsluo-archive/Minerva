50259f96c0e4d2145748f18cbebf21f51999d409
David Luo <mail@dsluo.me>
2018-12-11 00:44:31 -0500

    made bulletin views more readable

--- bulletin/views.py
+++ bulletin/views.py
@@ -1,101 +1,38 @@
 from django.shortcuts import render
 
-from django.db.models.functions import Lower
-
-from base.models import SubjectArea
 from base.models import Course
+from base.models import SubjectArea
 
 
-# Create your views here.
-
-
-# Displays the index of the bulletin view. Includes a list of subject areas.
 def index(request):
-    # Obtain a list of subject areas from the database.
-    list_of_subjects = SubjectArea.objects.order_by('long')
-
-    # Create a dictionary containing the list.
-    context = {
-        'list_of_subjects': list_of_subjects,
-    }
-
-    # Pass the dictionary into the template for the index page.
-    return render(request, 'bulletin/index.html', context)
+    """
+    Lists all subject areas
+    """
+    subjects = SubjectArea.objects.order_by('long')
+    return render(request, 'bulletin/index.html', {'subjects': subjects})
 
 
-# Displays the details page for a subject.
-# This view includes a description of the subject and list of classes belonging
-# to the subject.
 def subject(request, subject_short):
-
-    # Attempt to retrieve the subject object corresponding to the value passed
-    # into the function by the URL.
-    subject_object_list = SubjectArea.objects.filter(short__iexact=subject_short)
-
-    if subject_object_list.count() == 0:  # then the subject doesn't exist.
-
-        # Renders the template without a subject object. Template will display
-        # an error message to the user.
+    """
+    Lists all courses for a given subject
+    :param subject_short: the 4 letter subject abbreviation
+    """
+    try:
+        subject = SubjectArea.objects.get_or_create(short__iexact=subject_short)
+        courses = Course.objects.filter(subject_area=subject)
+        return render(request, 'bulletin/subject.html', {'subject': subject, 'courses': courses})
+    except SubjectArea.DoesNotExist:
         return render(request, 'bulletin/subject.html')
 
-    else:  # the subject does exist.
 
-        # Obtain a list of each course belonging to the subject.
-        list_of_courses = Course.objects.filter(subject_area__id=subject_object_list[0].id)
-
-        # Create a dictionary containing the subject object and the list of courses.
-        context = {
-            'subject': subject_object_list[0],
-            'list_of_courses' : list_of_courses
-        }
-
-        # Pass the object into the template for the subject view page.
-        return render(request, 'bulletin/subject.html', context)
-
-
-# Displays the details page for a single subject.
 def course(request, subject_short, course_number):
+    try:
+        subject_area = SubjectArea.objects.get(short__iexact=subject_short)
 
-    # Attempt to retrieve the subject object corresponding to the value passed
-    # into the function by the URL.
-    subject_object_list = SubjectArea.objects.filter(short__iexact=subject_short)
-
-    if subject_object_list.count() == 0 :  # then the subject doesn't exist.
-
-        # Create a dictionary with an error string.
-        error = "The requested subject doesn't exist."
-        context = {
-            'error': error
-        }
-
-        # Render the template with the dictionary. Error string will be displayed.
-        return render(request, 'bulletin/course.html', context)
-
-    else:  # the subject does exist.
-
-        # Retrieve all Courses categorized under that Subject Area.
-        course_object_list = Course.objects.filter(subject_area__id=subject_object_list[0].id)
-        course_object_list = course_object_list.filter(course_number__iexact=course_number)
-
-        if course_object_list.count() == 0:  # then the course doesn't exist.
-
-            # Create a dictionary with an error string.
-            error = "The requested course doesn't exist."
-            context = {
-                'error': error
-            }
-
-            # Render the template with the dictionary. Error string will be
-            # displayed.
-            return render(request, 'bulletin/course.html', context)
-
-        else:  # the course does exist.
-
-            # Create a dictionary containing the course object.
-            context = {
-                'course': course_object_list[0]
-            }
-
-            # Pass the object into the template for the course view page.
-            return render(request, 'bulletin/course.html', context)
-
+        try:
+            course = Course.objects.get(subject_area=subject_area, course_number=course_number)
+            return render(request, 'bulletin/course.html', {'course': course})
+        except Course.DoesNotExist:
+            return render(request, 'bulletin/course.html', {'error': "The requested course doesn't exist."})
+    except SubjectArea.DoesNotExist:
+        return render(request, 'bulletin/course.html', {'error': "The requested subject doesn't exist."})
