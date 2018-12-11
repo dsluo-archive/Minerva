faa30a4d45444b38cf307721374a09fde8aa0339
David Luo <mail@dsluo.me>
2018-12-11 06:27:37 -0500

    correct home url

--- minerva/urls.py
+++ minerva/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    # path('', include(), name='home'), todo
+    path('', RedirectView.as_view(pattern_name='schedule:schedule'), name='home'),
     path('admin/', admin.site.urls),
     path('bulletin/', include('bulletin.urls')),
     path('users/', include('users.urls')),
